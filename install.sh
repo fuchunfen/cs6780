3e7accb59f35fb17750ec75d27837a337c9d42ac
Kevin James Matzen <kmatzen@cs.cornell.edu>
2010-11-18 21:11:30 -0800

    Update installation script

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ cmake ..
 make
 cd ../../..
 
-cp OpenKinect-libfreenect-67c0d07/c/include/libfreenect.h KinectCapture/include
-cp OpenKinect-libfreenect-67c0d07/c/build/lib/libfreenect.a KinectCapture/lib
+cp OpenKinect-libfreenect-67c0d07/c/include/libfreenect.h KinectCapture
+cp OpenKinect-libfreenect-67c0d07/c/build/lib/libfreenect.a KinectCapture
 
 cd KinectCapture
 make
