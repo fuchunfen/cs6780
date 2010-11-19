6ffbd2549cb08ece7daeb3971ea2ee3586e68f83
Kevin James Matzen <kmatzen@cs.cornell.edu>
2010-11-18 16:08:00 -0800

    Decided not to store all of this in the repo

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+tar -zxvf 3rdparty/bundler-v0.4-source.tar.gz
+unzip 3rdparty/siftDemoV4.zip
+tar -zxvf 3rdparty/OpenKinect-libfreenect-67c0d07.tar.gz
+
 cd bundler-v0.4-source
-cp bin/libANN_char.so /usr/lib
+sudo cp bin/libANN_char.so /usr/lib
 make
 cd ..
 
+cd OpenKinect-libfreenect-67c0d07/c
+mkdir build
+cd build
+cmake ..
+make
+cd ../../..
